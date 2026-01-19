809d9225c1762cf502afb38c3faaf55a3fc1ae91
Beka Egod Zakaidze <bekaz@adwise.ge>
2026-01-20 02:56:01 +0400

    Configure GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/SolanaSpin/',
+  base: '/solanaspin/',
   plugins: [react()],
   define: {
     'process.env': {},
